6181fa95aa3776822a655758568f085f2f3198fb
Alexey Zarakovskiy <azarakovskiy@sdl.com>
2017-03-21 17:59:41 +0100

    TSI-2348 enable ADF filter for Model Service (TSI-24)

--- java/dxa-model-service/src/main/java/com/sdl/dxa/DxaModelServiceApplication.java
+++ java/dxa-model-service/src/main/java/com/sdl/dxa/DxaModelServiceApplication.java
@@ -1,9 +1,11 @@
 package com.sdl.dxa;
 
+import com.sdl.web.ambient.client.AmbientClientFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.ansi.AnsiOutput;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 
 @EnableCaching
 @SpringBootApplication
@@ -12,5 +14,11 @@ public class DxaModelServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(DxaModelServiceApplication.class, args);
         AnsiOutput.setEnabled(AnsiOutput.Enabled.ALWAYS);
+    }
+
+
+    @Bean
+    public AmbientClientFilter ambientClientFilter() {
+        return new AmbientClientFilter();
     }
 }
