b42c6036870284a31c1534737d56609da8f413a4
Alexey Zarakovskiy <azarakovskiy@sdl.com>
2017-03-29 15:05:45 +0200

    TSI-2385, TSI-2340 Merged assembly and controller artifacts into one (TSI-2371)

---
+++ java/dxa-model-service/src/main/java/com/sdl/dxa/DxaModelServiceApplication.java
@@ -0,0 +1,26 @@
+package com.sdl.dxa;
+
+import com.sdl.web.ambient.client.AmbientClientFilter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.ansi.AnsiOutput;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+
+@EnableCaching
+@SpringBootApplication
+public class DxaModelServiceApplication {
+
+    /**
+     * The main method stays here only because it is needed for development. Should not affect the application.
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(DxaModelServiceApplication.class, args);
+        AnsiOutput.setEnabled(AnsiOutput.Enabled.ALWAYS);
+    }
+
+    @Bean
+    public AmbientClientFilter ambientClientFilter() {
+        return new AmbientClientFilter();
+    }
+}
