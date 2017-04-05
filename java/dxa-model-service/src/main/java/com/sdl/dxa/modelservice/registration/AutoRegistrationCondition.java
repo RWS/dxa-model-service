e208c6677aaaf7b8c5a79e9c0c01a83a0d6e8b42
Alexey Zarakovskiy <azarakovskiy@sdl.com>
2017-04-05 15:26:27 +0200

    Fixed problems with auto registration and logback configuration

--- java/dxa-model-service/src/main/java/com/sdl/dxa/modelservice/registration/AutoRegistrationCondition.java
+++ java/dxa-model-service/src/main/java/com/sdl/dxa/modelservice/registration/AutoRegistrationCondition.java
@@ -1,5 +1,6 @@
 package com.sdl.dxa.modelservice.registration;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Condition;
 import org.springframework.context.annotation.ConditionContext;
 import org.springframework.core.type.AnnotatedTypeMetadata;
@@ -7,11 +8,14 @@ import org.springframework.core.type.AnnotatedTypeMetadata;
 /**
  * Condition to check whether all the dependencies and flags are present/enabled in order to proceed with auto-registration.
  */
+@Slf4j
 public class AutoRegistrationCondition implements Condition {
 
     @Override
     public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
-        return context.getResourceLoader().getResource(ModelServiceRegisterer.CONFIG_FILE_NAME).exists()
-                && context.getEnvironment().containsProperty("auto-register");
+        boolean matches = context.getResourceLoader().getResource(ModelServiceRegisterer.CONFIG_FILE_NAME).exists()
+                && context.getEnvironment().containsProperty("register");
+        log.debug("Auto registration of MS: {}", matches);
+        return matches;
     }
 }
