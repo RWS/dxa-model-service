7ea2230f41805190225ffd6c9c4df5626c23038b
Alexey Zarakovskiy <azarakovskiy@sdl.com>
2018-03-15 11:15:36 +0100

    CRQ-9041 Add remote IP filter as a bean

--- java/dxa-model-service/dxa-model-service-controller/src/main/java/com/sdl/dxa/DxaModelServiceApplication.java
+++ java/dxa-model-service/dxa-model-service-controller/src/main/java/com/sdl/dxa/DxaModelServiceApplication.java
@@ -6,6 +6,8 @@ import com.sdl.web.api.dynamic.taxonomies.WebTaxonomyFactory;
 import com.sdl.web.api.taxonomies.WebTaxonomyFactoryImpl;
 import com.tridion.ambientdata.web.AmbientDataServletFilter;
 import com.tridion.taxonomies.TaxonomyRelationManager;
+import org.apache.catalina.valves.RemoteIpValve;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.ansi.AnsiOutput;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -19,6 +21,18 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @PropertySource("classpath:dxa.properties")
 @Import({TridionCacheConfiguration.class, Dd4tSpringConfiguration.class})
 public class DxaModelServiceApplication {
+
+    @Value("${server.tomcat.protocol-header}")
+    private String ipFilterProtocolHeader;
+
+    @Value("${server.tomcat.port-header}")
+    private String ipFilterPortHeader;
+
+    @Value("${server.tomcat.remote-ip-header}")
+    private String ipFilterRemoteIpHeader;
+
+    @Value("${server.tomcat.internal-proxies}")
+    private String ipFilterInternalProxies;
 
     /**
      * The main method stays here only because it is needed for development. Should not affect the application.
@@ -47,4 +61,15 @@ public class DxaModelServiceApplication {
     public TaxonomyRelationManager taxonomyRelationManager() {
         return new TaxonomyRelationManager();
     }
+
+    @Bean
+    public RemoteIpValve remoteIpFilter() {
+        RemoteIpValve remoteIpFilter = new RemoteIpValve();
+        remoteIpFilter.setPortHeader(ipFilterPortHeader);
+        remoteIpFilter.setProtocolHeader(ipFilterProtocolHeader);
+        remoteIpFilter.setRemoteIpHeader(ipFilterRemoteIpHeader);
+        remoteIpFilter.setInternalProxies(ipFilterInternalProxies);
+        return remoteIpFilter;
+    }
+
 }
