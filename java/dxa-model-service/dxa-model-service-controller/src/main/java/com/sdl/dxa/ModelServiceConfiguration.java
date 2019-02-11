fc185b7c38db13be1c930ebf6ea62bdfd33e37c3
Raimond Kempees <kempees@radagio.com>
2019-02-11 16:14:43 +0100

    Use the right objectmapper.

--- java/dxa-model-service/dxa-model-service-controller/src/main/java/com/sdl/dxa/ModelServiceConfiguration.java
+++ java/dxa-model-service/dxa-model-service-controller/src/main/java/com/sdl/dxa/ModelServiceConfiguration.java
@@ -1,11 +1,15 @@
 package com.sdl.dxa;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sdl.dxa.caching.LocalizationIdProvider;
 import com.sdl.dxa.modelservice.ModelServiceLocalizationIdProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 
 /**
  * KeyGeneratorConfiguration.
@@ -15,8 +19,18 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 @EnableCaching
 public class ModelServiceConfiguration {
 
+    @Autowired
+    private ApplicationContext applicationContext;
+
     @Bean
     public LocalizationIdProvider localizationIdProvider () {
         return new ModelServiceLocalizationIdProvider();
+    }
+
+    @Bean
+    public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
+        MappingJackson2HttpMessageConverter jsonConverter = new MappingJackson2HttpMessageConverter();
+        jsonConverter.setObjectMapper(applicationContext.getBean(ObjectMapper.class));
+        return jsonConverter;
     }
 }
