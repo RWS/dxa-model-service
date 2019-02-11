72668e2c169139ff5bd9a70dcc8995b03532a3fe
Raimond Kempees <kempees@radagio.com>
2019-02-11 10:33:39 +0200

    CRQ-13104 > Proper timing.

--- java/dxa-model-service/dxa-model-service-controller/src/main/java/com/sdl/dxa/modelservice/service/DefaultPageModelService.java
+++ java/dxa-model-service/dxa-model-service-controller/src/main/java/com/sdl/dxa/modelservice/service/DefaultPageModelService.java
@@ -108,8 +108,10 @@ public class DefaultPageModelService implements PageModelService, LegacyPageMode
 
         String pageContent = contentService.loadPageContent(pageRequest);
         log.trace("Loaded page content for {}", pageRequest);
+
+        final PageModelData pageModelData = _processR2PageModel(pageContent, pageRequest);
         TimerLogger.log("Load Page Content", (System.currentTimeMillis() - start));
-        return _processR2PageModel(pageContent, pageRequest);
+        return pageModelData;
     }
 
     private List<EntityModelData> _expandDynamicEntities(@NotNull RegionModelData region, int publicationId) throws ContentProviderException {
