80ad0747c0e37a02eb28404d472c7816b52119c4
mborysenko <mborysenko@sdl.com>
2019-02-24 17:34:50 +0200

    CRQ-13104 > Fixed issue when mapper processed json twice making it invalid

--- java/dxa-model-service/dxa-model-service-controller/src/main/java/com/sdl/dxa/modelservice/controller/PageModelController.java
+++ java/dxa-model-service/dxa-model-service-controller/src/main/java/com/sdl/dxa/modelservice/controller/PageModelController.java
@@ -91,7 +91,7 @@ public class PageModelController {
 
             result = dataModelType == DataModelType.R2 ?
                     pageModelService.loadPageModel(pageRequestDto) :
-                    JsonDataBinder.getGenericMapper().writeValueAsString(legacyPageModelService.loadLegacyPageModel(pageRequestDto));
+                    legacyPageModelService.loadLegacyPageModel(pageRequestDto);
         }
         return ResponseEntity.ok(result);
     }
