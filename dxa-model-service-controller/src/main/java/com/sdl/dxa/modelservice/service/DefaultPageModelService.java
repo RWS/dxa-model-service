99f5904cba7065539218368befde050360a72d99
Igor Nikonov <inikonov@sdl.com>
2020-07-17 10:41:16 +0300

    CRQ-20076 removed excessive caching which causes livelock in ehcache

--- dxa-model-service-controller/src/main/java/com/sdl/dxa/modelservice/service/DefaultPageModelService.java
+++ dxa-model-service-controller/src/main/java/com/sdl/dxa/modelservice/service/DefaultPageModelService.java
@@ -93,7 +93,6 @@ public class DefaultPageModelService implements PageModelService, LegacyPageMode
 
     @Override
     @NotNull
-    @Cacheable(value = "pageModels", key = "{ #root.methodName, #pageRequest }", sync = true)
     public Page loadLegacyPageModel(PageRequestDto pageRequest) throws ContentProviderException {
         String pageContent = contentService.loadPageContent(pageRequest);
         log.trace("Loaded page content for {}", pageRequest);
@@ -102,7 +101,6 @@ public class DefaultPageModelService implements PageModelService, LegacyPageMode
 
     @Override
     @NotNull
-    @Cacheable(value = "pageModels", key = "{ #root.methodName, #pageRequest }", sync = true)
     public PageModelData loadPageModel(PageRequestDto pageRequest) throws ContentProviderException {
         String pageContent = contentService.loadPageContent(pageRequest);
         log.trace("Loaded page content for {}", pageRequest);
